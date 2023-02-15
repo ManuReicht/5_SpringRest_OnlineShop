6dbd85c664cc484653c196164b773c7c61b4f74e
Manuel Reicht <reichtmanuel01@gmail.com>
2023-02-15 09:35:31 +0100

    initial commit

---
+++ src/test/java/at/kaindorf/onlineshop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kaindorf.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
